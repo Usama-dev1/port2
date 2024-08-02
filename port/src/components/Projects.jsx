680e93de00a5d0f6181e5f8b0d73dd909b1c4292
Usama dev <154013836+Usama-dev1@users.noreply.github.com>
2024-08-02 15:21:11 +0500

    Update Projects.jsx

--- port/src/components/Projects.jsx
+++ port/src/components/Projects.jsx
@@ -7,7 +7,32 @@ import {
   project2,
   project3,
 } from "../assets/index";
+import ProjectCard from "./ProjectCard";
 const Projects = () => {
+  const projects=[
+    {
+     projectImg:project1,
+     projectTitle:"Project Title1",
+     projectDesc:"Project Description1",
+     projectExtra:"Project Extra info URL 1",
+     projectUrl:"Project URL 1"
+    },
+    {
+     projectImg:project2,
+     projectTitle:"Project Title2",
+     projectDesc:"Project Description 2",
+     projectExtra:"Project Extra info URL 2",
+     projectUrl:"Project URL 2"
+    },
+    {
+     projectImg:project3,
+     projectTitle:"Project Title3",
+     projectDesc:"Project Description 3",
+     projectExtra:"Project Extra info URL 3",
+     projectUrl:"Project URL 3"
+    },
+    
+  ]
   return (
     <>
       <section
@@ -35,157 +60,15 @@ const Projects = () => {
 
           {/* <!-- Projects --> */}
           <div className="px-6 py-4 md:mx-8 lg:mx-16">
-            {/* <!-- Project 1 --> */}
-            <div className="py-12 md:pt-16 lg:grid lg:grid-cols-2 lg:gap-x-16 lg:h-96 lg:py-0 lg:mt-24 lg:mb-32">
-              {/* <!-- Project Image --> */}
-              <div className="lg:relative lg:pb-2/3">
-                <img
-                  className="shadow-lg md:w-full lg:h-full lg:object-cover lg:object-top lg:absolute"
-                  src={project1}
-                  alt="Example page from project"
-                />
-              </div>
-
-              {/* <!-- Project Details --> */}
-              <div className="lg:h-full lg:flex lg:items-center">
-                <div className="lg:grid lg:auto-rows-min">
-                  {/* <!-- Project name --> */}
-                  <div className="inline-block relative">
-                    <div className="lg:relative lg:inline-block">
-                      <h3 className="relative z-10 font-bold pt-8 md:text-xl">
-                        Project Name
-                      </h3>
-                      <div className="absolute bottom-0 h-2 w-full bg-accent"></div>
-                    </div>
-                  </div>
-
-                  {/* <!-- Project description --> */}
-                  <p className="py-4 text-sm leading-relaxed md:text-base lg:py-6">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nibh gravida gravida elementum consectetur facilisis vel
-                    quam ullamcorper. Condimentum sed ultricies tellus et augue.
-                    Lacus eu lobortis scelerisque vitae et. Vestibulum arcu
-                    maecenas imperdiet egestas lectus.
-                  </p>
-
-                  {/* <!-- Project URL + extra info  --> */}
-                  <span className="inline-block italic opacity-80 text-sm mb-8">
-                    projectname.co.za - Design + Code + Marketing
-                  </span>
-                  <br className="lg:hidden" />
-
-                  {/* <!-- CTA --> */}
-                  <a
-                    href="#"
-                    className="inline-block uppercase font-medium bg-accent px-4 py-2 text-black shadow-md ml-auto lg:ml-0 lg:w-max">
-                    <span className="text-sm opacity-75">Visit Project</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            {/* <!-- Project 2 --> */}
-            <div className="py-12 md:pt-16 lg:grid lg:grid-cols-2 lg:gap-x-16 lg:h-96 lg:py-0 lg:my-32">
-              {/* <!-- Project Image --> */}
-              <div className="lg:relative lg:pb-2/3 lg:col-start-2">
-                <img
-                  className="shadow-lg md:w-full lg:h-full lg:object-cover lg:object-top lg:absolute z-10"
-                  src={project2}
-                  alt="Example page from project"
-                />
-                <img
-                  className="hidden z-0 lg:inline-block lg:absolute lg:right-20 lg:transform lg:scale-105"
-                  src={accimg2}
-                  alt="Red decorative bubble."
-                />
-              </div>
-
-              {/* <!-- Project Details --> */}
-              <div className="lg:h-full lg:flex lg:items-center lg:row-start-1">
-                <div className="lg:grid lg:auto-rows-min">
-                  {/* <!-- Project name --> */}
-                  <div className="inline-block relative">
-                    <div className="lg:relative lg:inline-block">
-                      <h3 className="relative z-10 font-bold pt-8 md:text-xl">
-                        Project Name
-                      </h3>
-                      <div className="absolute bottom-0 h-2 w-full bg-accent"></div>
-                    </div>
-                  </div>
-
-                  {/* <!-- Project description --> */}
-                  <p className="py-4 text-sm leading-relaxed md:text-base lg:py-6">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nibh gravida gravida elementum consectetur facilisis vel
-                    quam ullamcorper. Condimentum sed ultricies tellus et augue.
-                    Lacus eu lobortis scelerisque vitae et. Vestibulum arcu
-                    maecenas imperdiet egestas lectus.
-                  </p>
-
-                  {/* <!-- Project URL + extra info  --> */}
-                  <span className="inline-block italic opacity-80 text-sm mb-8">
-                    projectname.co.za - Design + Code + Marketing
-                  </span>
-                  <br className="lg:hidden" />
-
-                  {/* <!-- CTA --> */}
-                  <a
-                    href="#"
-                    className="inline-block uppercase font-medium bg-accent px-4 py-2 text-black shadow-md ml-auto lg:ml-0 lg:w-max">
-                    <span className="text-sm opacity-75">Visit Project</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            {/* <!-- Project 3 --> */}
-            <div className="py-12 md:pt-16 lg:grid lg:grid-cols-2 lg:gap-x-16 lg:h-96 lg:py-0 lg:my-32">
-              {/* <!-- Project Image --> */}
-              <div className="lg:relative lg:pb-2/3">
-                <img
-                  className="shadow-lg md:w-full lg:h-full lg:object-cover lg:object-top lg:absolute"
-                  src={project3}
-                  alt="Example page from project"
-                />
-              </div>
-
-              {/* <!-- Project Details --> */}
-              <div className="lg:h-full lg:flex lg:items-center">
-                <div className="lg:grid lg:auto-rows-min">
-                  {/* <!-- Project name --> */}
-                  <div className="inline-block relative">
-                    <div className="lg:relative lg:inline-block">
-                      <h3 className="relative z-10 font-bold pt-8 md:text-xl">
-                        Project Name
-                      </h3>
-                      <div className="absolute bottom-0 h-2 w-full bg-accent"></div>
-                    </div>
-                  </div>
-
-                  {/* <!-- Project description --> */}
-                  <p className="py-4 text-sm leading-relaxed md:text-base lg:py-6">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nibh gravida gravida elementum consectetur facilisis vel
-                    quam ullamcorper. Condimentum sed ultricies tellus et augue.
-                    Lacus eu lobortis scelerisque vitae et. Vestibulum arcu
-                    maecenas imperdiet egestas lectus.
-                  </p>
-
-                  {/* <!-- Project URL + extra info  --> */}
-                  <span className="inline-block italic opacity-80 text-sm mb-8">
-                    projectname.co.za - Design + Code + Marketing
-                  </span>
-                  <br className="lg:hidden" />
-
-                  {/* <!-- CTA --> */}
-                  <a
-                    href="#"
-                    className="inline-block uppercase font-medium bg-accent px-4 py-2 text-black shadow-md ml-auto lg:ml-0 lg:w-max">
-                    <span className="text-sm opacity-75">Visit Project</span>
-                  </a>
-                </div>
-              </div>
-            </div>
+            {projects.map((project) => (
+              <ProjectCard
+                projectImg={project.projectImg}
+                projectTitle={project.projectTitle}
+                projectDesc={project.projectDesc}
+                projectExtra={project.projectExtra}
+                projectUrl={project.projectUrl}
+              />
+            ))}
           </div>
 
           {/* <!-- Corner Bubble --> */}
