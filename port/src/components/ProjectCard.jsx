805befd8228c87de64ca81e96743627996f63148
Usama dev <154013836+Usama-dev1@users.noreply.github.com>
2024-08-03 11:47:14 +0500

    Update ProjectCard.jsx

--- port/src/components/ProjectCard.jsx
+++ port/src/components/ProjectCard.jsx
@@ -8,13 +8,14 @@ const ProjectCard = ({projectImg,
     <>
       <div className="w-full py-12 md:pt-16 lg:grid lg:grid-cols-2 lg:gap-x-16 lg:h-96 lg:py-0 lg:my-32">
   {/* <!-- Project Image --> */}
-  <div className="w-120 h-96 lg:h-full lg:relative">
+<div className="w-full lg:relative" style={{ paddingBottom: '56.25%' }}>
   <img
-    className="w-full h-full object-cover shadow-lg lg:absolute"
+    className="absolute inset-0 w-full h-full object-cover shadow-lg"
     src={projectImg}
     alt="Example page from project"
   />
 </div>
+
 
 
         {/* <!-- Project Details --> */}
