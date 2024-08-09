28afe943bb84722427c3fed9048ca7735ec35d6a
Usama dev <154013836+Usama-dev1@users.noreply.github.com>
2024-08-09 16:57:06 +0500

    Update vite.config.js

--- port/vite.config.js
+++ port/vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'port' // or 'dist' if that’s what you use
-  },
   server: {
     proxy: {
       '/api': {
